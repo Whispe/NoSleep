badd03cfe43df619e4821d5840ff51f2e4043842
Tifany Yung <tyung1@jhu.edu>
2014-04-02 14:08:36 -0400

    put in stochastic k-means

--- LambdaMeansPredictor.java
+++ LambdaMeansPredictor.java
@@ -22,22 +22,22 @@ public class LambdaMeansPredictor extends Predictor {
 		this.numIters = clustering_training_iterations;
 		FeatureSelector igSelector = new FeatureSelector();
 		this.numFeatures = igSelector.countFeatures(instances);
-		double[] mu1 = new double[numFeatures];
+		double[] mu1 = new double[this.numFeatures];
 		
 		// Initialize mu1 to zero vector.
-		for (int i=0; i<numFeatures; i++) {
+		for (int i=0; i<this.numFeatures; i++) {
 			
 			mu1[i] = 0;
 		}
 		
 		// Find the average distance of each training instance to mean of training data.
-		for (int i=0; i<numInstances; i++) {
+		for (int i=0; i<this.numInstances; i++) {
 			
-			double[] x = instances.get(i).getFeatureVector().getVectorAsArray(numFeatures);
+			double[] x = instances.get(i).getFeatureVector().getVectorAsArray(this.numFeatures);
 			mu1 = Vector.sumVectors(mu1, x);
 		}
-		mu1 = Vector.scalarMult(1.0/numInstances, mu1);
-		mu_k.add(mu1);
+		mu1 = Vector.scalarMult(1.0/this.numInstances, mu1);
+		this.mu_k.add(mu1);
 		
 		// Set lambda to average distance of each training instance to mean of training data
 		// if cluster_lambda is 0 (not given in command line).
@@ -47,25 +47,25 @@ public class LambdaMeansPredictor extends Predictor {
 		double sum = 0;
 		if (cluster_lambda == 0) {
 			
-			for (int i=0; i<numInstances; i++) {
+			for (int i=0; i<this.numInstances; i++) {
 				
-				double[] x = instances.get(i).getFeatureVector().getVectorAsArray(numFeatures);
+				double[] x = instances.get(i).getFeatureVector().getVectorAsArray(this.numFeatures);
 				sum += Vector.dist(x, mu1);
 			}
 			this.lambda = sum;
 			//System.out.println("Lambda without norm: " + this.lambda);
-			this.lambda = sum / numInstances;
+			this.lambda = sum / this.numInstances;
 			System.out.println("Lambda with norm: " + this.lambda);
 		}
 		
-		r_nk.add(new ArrayList<Integer>());
+		this.r_nk.add(new ArrayList<Integer>());
 		
 		train(instances);
 	}
 
 	public void train(List<Instance> instances) {
 		
-		for (int i=0; i<numIters; i++) {
+		for (int i=0; i<this.numIters; i++) {
 			EStep(instances);
 			MStep(instances);
 		}
@@ -73,20 +73,20 @@ public class LambdaMeansPredictor extends Predictor {
 	
 	public void EStep(List<Instance> instances) {
 		
-		for (int i=0; i<r_nk.size(); i++) {
+		for (int i=0; i<this.r_nk.size(); i++) {
 			
-			r_nk.get(i).clear();
+			this.r_nk.get(i).clear();
 		}
 		
-		for (int i=0; i<numInstances; i++) {
+		for (int i=0; i<this.numInstances; i++) {
 			
 			double minDist = Double.POSITIVE_INFINITY;
 			int min_k = 0;
-			double[] x_i = instances.get(i).getFeatureVector().getVectorAsArray(numFeatures);
+			double[] x_i = instances.get(i).getFeatureVector().getVectorAsArray(this.numFeatures);
 				
-			for (int k=0; k<mu_k.size(); k++) {
+			for (int k=0; k<this.mu_k.size(); k++) {
 				
-				double dist = Vector.dist(mu_k.get(k), x_i);
+				double dist = Vector.dist(this.mu_k.get(k), x_i);
 				if (dist < minDist) {
 					
 					minDist = dist;
@@ -95,50 +95,47 @@ public class LambdaMeansPredictor extends Predictor {
 			}
 			if (minDist <= this.lambda) {
 				
-				r_nk.get(min_k).add(i);
-				//System.out.println("Went to cluster: " + min_k);
+				this.r_nk.get(min_k).add(i);
 			}
 			else {
-				//System.out.println("Distance: " + minDist);
 				ArrayList<Integer> newCluster = new ArrayList<Integer>();
 				newCluster.add(i);
-				r_nk.add(newCluster);
-				mu_k.add(x_i);
-				//System.out.println("New cluster: " + mu_k.size());
+				this.r_nk.add(newCluster);
+				this.mu_k.add(x_i);
 			}
 		}
 	}
 	
 	public void MStep(List<Instance> instances) {
 		
-		for (int k=0; k<mu_k.size(); k++) {
+		for (int k=0; k<this.mu_k.size(); k++) {
 			
 			int numInClusterk = 0;
-			double[] newMu_k = new double[numFeatures];
+			double[] newMu_k = new double[this.numFeatures];
 			
 			// Initialize newMu_k to zero vector.
-			for (int i=0; i<numFeatures; i++) {
+			for (int i=0; i<this.numFeatures; i++) {
 				
 				newMu_k[i] = 0;
 			}
 			
-			for(int d : r_nk.get(k)) {
+			for(int d : this.r_nk.get(k)) {
 				
-				double[] x_i = instances.get(d).getFeatureVector().getVectorAsArray(numFeatures);
+				double[] x_i = instances.get(d).getFeatureVector().getVectorAsArray(this.numFeatures);
 				newMu_k = Vector.sumVectors(newMu_k, x_i);
 				numInClusterk++;
 			}
-			if (r_nk.get(k).size() == 0) {
+			if (this.r_nk.get(k).size() == 0) {
 				
-				newMu_k = new double[this.numFeatures];
-				for (int j=0; j<newMu_k.length; j++) {
+				//newMu_k = new double[this.numFeatures];
+				//for (int j=0; j<newMu_k.length; j++) {
 					
-					newMu_k[j] = 0;
-				}
-				mu_k.set(k, newMu_k);
+				//	newMu_k[j] = 0;
+				//}
+				this.mu_k.set(k, newMu_k);
 			} else {
 				newMu_k = Vector.scalarMult(1.0/numInClusterk, newMu_k);
-				mu_k.set(k, newMu_k);
+				this.mu_k.set(k, newMu_k);
 			}
 		}
 	}
@@ -147,11 +144,11 @@ public class LambdaMeansPredictor extends Predictor {
 		
 		double minDist = Double.POSITIVE_INFINITY;
 		int min_k = 0;
-		double[] x_i = instance.getFeatureVector().getVectorAsArray(numFeatures);
+		double[] x_i = instance.getFeatureVector().getVectorAsArray(this.numFeatures);
 		
-		for (int k=0; k<mu_k.size(); k++) {
+		for (int k=0; k<this.mu_k.size(); k++) {
 			
-			double dist = Vector.dist(x_i, mu_k.get(k));
+			double dist = Vector.dist(x_i, this.mu_k.get(k));
 			
 			if (dist < minDist) {
 				
--- StochasticKMeansPredictor.java
+++ StochasticKMeansPredictor.java
@@ -1,5 +1,6 @@
 package cs475;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,20 +9,129 @@ import java.util.List;
  */
 public class StochasticKMeansPredictor extends Predictor {
 
+	int numInstances;
+	int numIters;
+	int numFeatures;
+	int numClusters;
+	double[][] mu_k; // Length numClusters.
+	int[] whichCluster; // Length numInstances.
+	int[] perCluster; // Length numClusters.
+	
 	public StochasticKMeansPredictor(List<Instance> instances, int clustering_training_iterations, int num_clusters) {
 		
+		this.numInstances = instances.size();
+		this.numIters = clustering_training_iterations;
+		this.numClusters = num_clusters;
+		FeatureSelector igSelector = new FeatureSelector();
+		this.numFeatures = igSelector.countFeatures(instances);
 		
+		this.mu_k = new double[this.numClusters][this.numFeatures];
+		for (int i=0; i<this.mu_k.length ; i++) {
+			double[] x_i = instances.get(i).getFeatureVector().getVectorAsArray(this.numFeatures);
+			this.mu_k[i] = x_i;
+		}
+		
+		this.whichCluster = new int[this.numInstances];
+		for (int i=0; i<this.whichCluster.length; i++) {
+			this.whichCluster[i] = -1;
+		}
+		
+		this.perCluster = new int[this.numClusters];
+		for (int i=0; i<this.perCluster.length; i++) {
+			this.perCluster[i] = 0;
+		}
 	}
 
 	public void train(List<Instance> instances) {
 		
-		
+		for (int i=0; i<numIters; i++) {
+			
+			for (int j=0; j < numInstances; j++) {
+				
+				double minDist = Double.POSITIVE_INFINITY;
+				int min_k = 0;
+				double[] x_i = instances.get(i).getFeatureVector().getVectorAsArray(this.numFeatures);
+					
+				for (int k=0; k<this.mu_k.length; k++) {
+					
+					double dist = Vector.dist(this.mu_k[k], x_i);
+					if (dist < minDist) {
+						
+						minDist = dist;
+						min_k = k;
+					}
+				}
+				int old_k = whichCluster[i];
+				int new_k = min_k;
+				this.perCluster[old_k]--;
+				this.perCluster[new_k]++;
+				this.whichCluster[i] = new_k;
+				
+				// Update mu_k for old and new cluster.
+				double[] newMu_k = new double[this.numFeatures];
+				
+				// Old cluster first.
+				// Initialize newMu_k to zero vector.
+				for (int k=0; k<this.numFeatures; k++) {
+					
+					newMu_k[k] = 0;
+				}
+				
+				for (int k=0; k<this.whichCluster.length; k++) {
+					
+					if (this.whichCluster[k] == old_k) {
+						x_i = instances.get(k).getFeatureVector().getVectorAsArray(this.numFeatures);
+						newMu_k = Vector.sumVectors(newMu_k, x_i);
+					}
+				}
+				if (this.perCluster[old_k] == 0) {
+					this.mu_k[old_k] = newMu_k;
+				} else {
+					newMu_k = Vector.scalarMult(1.0/this.perCluster[old_k], newMu_k);
+					this.mu_k[old_k] = newMu_k;
+				}
+				
+				// New cluster next.
+				// Initialize newMu_k to zero vector.
+				for (int k=0; k<this.numFeatures; k++) {
+					
+					newMu_k[k] = 0;
+				}
+				
+				for (int k=0; k<this.whichCluster.length; k++) {
+					
+					if (this.whichCluster[k] == new_k) {
+						x_i = instances.get(k).getFeatureVector().getVectorAsArray(this.numFeatures);
+						newMu_k = Vector.sumVectors(newMu_k, x_i);
+					}
+				}
+				if (this.perCluster[new_k] == 0) {
+					this.mu_k[new_k] = newMu_k;
+				} else {
+					newMu_k = Vector.scalarMult(1.0/this.perCluster[new_k], newMu_k);
+					this.mu_k[new_k] = newMu_k;
+				}
+			}
+		}
 	}
 
 	public Label predict(Instance instance) {
 
+		double minDist = Double.POSITIVE_INFINITY;
+		int min_k = 0;
+		double[] x_i = instance.getFeatureVector().getVectorAsArray(this.numFeatures);
 		
+		for (int k=0; k<this.mu_k.length; k++) {
+			
+			double dist = Vector.dist(x_i, this.mu_k[k]);
+			
+			if (dist < minDist) {
+				
+				minDist = dist;
+				min_k = k;
+			}
+		}
 		
-		return null;
+		return new ClassificationLabel(min_k);
 	}
 }
